8764f99790e689fa5227c911f9f22e498ab41a41
umakannan3 <umaheshcbe3@gmail.com>
2024-03-06 22:00:40 +0530

    cannges

---
+++ kunal_Java/Fibonacci.java
@@ -0,0 +1,33 @@
+import java.util.Scanner;
+
+public class Fibonacci {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("n : ");
+        int n = sc.nextInt();
+        int a = 0;
+        int b = 1;
+        int count = 2;
+
+        // while (count <= n) {
+        // System.out.print( a + ' ');
+        // a = b;
+        // b = a+b;
+        // count++;
+        // }
+
+        while (count<=n) {
+            // System.out.print(a);
+            int c = a+b;
+            a = b;
+            b = c;
+            count++;
+            // a = b;
+            // b = a + b;
+            // System.out.print(" ");
+        }
+        System.out.print(b);
+        
+
+    }
+}
